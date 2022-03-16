7e0ec0ae5c79126b229de4bcfcb61335840d1077
Minnie Kasemkiatsakool <minniek@Minnies-Air.hub>
2022-03-16 14:56:02 +1300

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import React, {Component } from 'react';
 
-const App = () =>
-    <div className="App">
-      <h1>Welcome to CISE - the home of learning and fun</h1>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+  this.setState(prevState => ({
+    count: prevState.count + amount,
+  }));
+
+  increment = this.makeIncrement(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
       </div>
-  
+    )
+  }
+}
+
 export default App;
